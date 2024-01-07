1c2f99bf302c789e63ff0f7b40ff13eeb3a376d6
zhuxx <zhuxx@si-tech.com.cn>
2024-01-07 18:16:55 +0800

    0169 多数元素

---
+++ src/test/java/com/example/leetcode/LeetCode0169.java
@@ -0,0 +1,26 @@
+package com.example.leetcode;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 给定一个大小为 n 的数组 nums ，返回其中的多数元素。多数元素是指在数组中出现次数 大于 ⌊ n/2 ⌋ 的元素。
+ * <p>
+ * 你可以假设数组是非空的，并且给定的数组总是存在多数元素。
+ */
+public class LeetCode0169 {
+
+    @Test
+    public void test() {
+        System.out.println(majorityElement(new int[]{3, 2, 3}));
+        System.out.println(majorityElement(new int[]{2, 2, 1, 1, 1, 2, 2}));
+    }
+
+    public int majorityElement(int[] nums) {
+        Arrays.sort(nums);
+        return nums[nums.length / 2];
+    }
+}
