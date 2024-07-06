d7a97db2a08a6ce930a2686967c6866b17304f4e
zhuxx <zhuxiaoxia0215@qq.com>
2024-07-06 09:47:26 +0800

    0130 被围绕的区域

---
+++ src/test/java/com/example/leetcode/LeetCode0130.java
@@ -0,0 +1,51 @@
+package com.example.leetcode;
+
+/**
+ * 130. 被围绕的区域
+ * <p>
+ * 给你一个 m x n 的矩阵 board ，由若干字符 'X' 和 'O' 组成，捕获 所有 被围绕的区域：
+ * <p>
+ * 连接：一个单元格与水平或垂直方向上相邻的单元格连接。
+ * 区域：连接所有 '0' 的单元格来形成一个区域。
+ * 围绕：如果您可以用 'X' 单元格 连接这个区域，并且区域中没有任何单元格位于 board 边缘，则该区域被 'X' 单元格围绕。
+ * 通过将输入矩阵 board 中的所有 'O' 替换为 'X' 来 捕获被围绕的区域。
+ */
+public class LeetCode0130 {
+
+    public void solve(char[][] board) {
+        int n = board.length;
+        if (n < 2) {
+            return;
+        }
+        int m = board[0].length;
+        for (int i = 0; i < n; i++) {
+            dfs(board, i, 0);
+            dfs(board, i, m - 1);
+        }
+        for (int i = 0; i < m; i++) {
+            dfs(board, 0, i);
+            dfs(board, n - 1, i);
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (board[i][j] == 'A') {
+                    board[i][j] = 'O';
+                } else if (board[i][j] == 'O') {
+                    board[i][j] = 'X';
+                }
+            }
+        }
+    }
+
+    void dfs(char[][] board, int x, int y) {
+        if (x < 0 || x >= board.length || y < 0 || y >= board[0].length || board[x][y] != 'O') {
+            return;
+        }
+        board[x][y] = 'A';
+        dfs(board, x + 1, y);
+        dfs(board, x, y + 1);
+        dfs(board, x - 1, y);
+        dfs(board, x, y - 1);
+    }
+
+}
